c67608e33b233cafe8dd1ec7b6bb20cce02e4631
Zen <amaar5ali@gmail.com>
2022-06-01 01:19:07 -0400

    finishing the first draft

--- script.js
+++ script.js
@@ -12,30 +12,61 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    let paperbeatrockwin = "You win, Paper beats Rock";
-    let rockbeatscissorswin = "You win, Rock beats Scissors";
-    let scissorsbeatpaperwin = "You win, Scissors beats Paper";
-    let paperbeatrocklose = "You lose, Paper beats Rock";
-    let rockbeatscissorslose = "You lose, Rock beats Scissors";
-    let scissorsbeatpaperlose = "You lose, Scissors beats Paper";
-    let draw = "It's a draw, neither player won";
+function game() {
+    let roundsPlayed = 0;
+    let playerWin = 0;
+    let computerWin = 0;
+    let gameWinner = "";
 
-    if (playerSelection === computerSelection) {
-        return draw;
-    } else if ((playerSelection = "Paper") && (computerSelection = "Rock")) {
-        return paperbeatrockwin;
-    } else if ((playerSelection = "Rock") && (computerSelection = "Scissors")) {
-        return rockbeatscissorswin;
-    } else if ((playerSelection = "Scissors") && (computerSelection = "Paper")) {
-        return scissorsbeatpaperwin;
-    } else if ((computerSelection = "Paper") && (playerSelection = "Rock")) {
-        return paperbeatrocklose;
-    } else if ((computerSelection = "Rock") && (playerSelection = "Scissors")) {
-        return rockbeatscissorslose;
-    } else () {
-        return scissorsbeatpaperlose;
+    while (roundsPlayed < 5) {
+        roundsPlayed++;
+        const computerSelection = computerPlay();
+        playerSelection = prompt("Player, please type in your selection");
+        console.log("Player win totals " + playerWin);
+        console.log("Computer win totals " + computerWin);
+        
+        function playRound(playerSelection, computerSelection) {
+            let paperbeatrockwin = "You win, Paper beats Rock";
+            let rockbeatscissorswin = "You win, Rock beats Scissors";
+            let scissorsbeatpaperwin = "You win, Scissors beats Paper";
+            let paperbeatrocklose = "You lose, Paper beats Rock";
+            let rockbeatscissorslose = "You lose, Rock beats Scissors";
+            let scissorsbeatpaperlose = "You lose, Scissors beats Paper";
+            let draw = "It's a draw, neither player won";
+    
+            if (playerSelection === computerSelection) {
+            return draw;
+            } else if ((playerSelection = "Paper") && (computerSelection = "Rock")) {
+            playerWin++;
+            return paperbeatrockwin;
+            } else if ((playerSelection = "Rock") && (computerSelection = "Scissors")) {
+            playerWin++;
+            return rockbeatscissorswin;
+            } else if ((playerSelection = "Scissors") && (computerSelection = "Paper")) {
+            playerWin++;
+            return scissorsbeatpaperwin;
+            } else if ((computerSelection = "Paper") && (playerSelection = "Rock")) {
+            computerWin++;
+            return paperbeatrocklose;
+            } else if ((computerSelection = "Rock") && (playerSelection = "Scissors")) {
+            computerWin++;
+            return rockbeatscissorslose;
+            } else () {
+            computerWin++;
+            return scissorsbeatpaperlose;
+            }
+        }   
     }
+
+    if (playerWin > computerWin) {
+        gameWinner = "YOU WIN";
+    } else if (playerWin === computerWin) {
+        gameWinner = "IT IS A DRAW";
+    } else {
+        gameWinner = "COMPUTER WINS";
+    }
+
+    console.log("The final winner is: " + gameWinner);
 }
 
-// Function to null case-sensitivity for inputs
+game();
