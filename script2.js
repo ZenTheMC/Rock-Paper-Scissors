ab618ee1e100ca79693d4eab437ef96d077356e3
Zen <amaar5ali@gmail.com>
2022-06-15 00:30:28 -0400

    Fixed rounds played from 6 to 5 total by i=6

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ const choices = ["rock","paper","scissors"];
 const winners = [];
 
 function game() {
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     playRound(i);
   }
   document.querySelector("button").textContent = "Play new game";
